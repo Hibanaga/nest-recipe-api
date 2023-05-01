e65c772b5789f513d29a9740980be2d108702c78
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-05-01 10:53:02 +0200

    Add validation pipes to controllers

--- src/src/modules/recipe/dto/create-recipe.dto.ts
+++ src/src/modules/recipe/dto/create-recipe.dto.ts
@@ -1,13 +1,15 @@
-export class CreateRecipeDto {
-  description: string;
-  ingridients: IngredientDto[];
-}
-
-export class IngredientDto {
-  name: string;
-  unit: Unit;
-  quantity: number;
-}
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export enum Unit {
   MILILITERS = 'mililiters',
@@ -17,4 +19,32 @@ export enum Unit {
   SPOONS = 'spoons',
   CUPS = 'cups',
   PIECES = 'pieces',
+}
+
+export class CreateRecipeDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => IngredientDto)
+  ingredients: IngredientDto[];
+}
+
+export class IngredientDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(Unit)
+  unit: Unit;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  quantity: number;
 }
