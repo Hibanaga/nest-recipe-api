c909634dc64df3a89c743b92dfaab45e9ebda7e1
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-05-02 08:21:30 +0200

    Fix Migrations errors with already exists migrations

--- src/migrations/1682934536468-initial-schema.ts
+++ src/migrations/1682934536468-initial-schema.ts
@@ -9,76 +9,80 @@ import { Unit } from 'src/modules/recipe/dto/create-recipe.dto';
 
 export class InitialSchema1682934536468 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: 'recipe',
-        columns: [
-          {
-            name: 'id',
-            type: 'uuid',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'uuid',
-            isNullable: false,
-          },
-          {
-            name: 'description',
-            type: 'varchar',
-            isNullable: false,
-          },
-        ],
-      }),
-    );
+    if (!(await queryRunner.hasTable('recipe'))) {
+      await queryRunner.createTable(
+        new Table({
+          name: 'recipe',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'uuid',
+              isNullable: false,
+            },
+            {
+              name: 'description',
+              type: 'varchar',
+              isNullable: false,
+            },
+          ],
+        }),
+      );
+    }
 
-    await queryRunner.createTable(
-      new Table({
-        name: 'ingredient',
-        columns: [
-          {
-            name: 'id',
-            type: 'uuid',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'uuid',
-            isNullable: false,
-          },
-          {
-            name: 'name',
-            type: 'varchar',
-            isNullable: false,
-          },
-          {
-            name: 'unit',
-            type: 'varchar',
-            isNullable: false,
-            enum: [...Object.values(Unit)],
-          },
-          {
-            name: 'quantity',
-            type: 'integer',
-            isNullable: false,
-          },
-        ],
-      }),
-    );
+    if (!(await queryRunner.hasTable('ingredient'))) {
+      await queryRunner.createTable(
+        new Table({
+          name: 'ingredient',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'uuid',
+              isNullable: false,
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+              isNullable: false,
+            },
+            {
+              name: 'unit',
+              type: 'varchar',
+              isNullable: false,
+              enum: [...Object.values(Unit)],
+            },
+            {
+              name: 'quantity',
+              type: 'integer',
+              isNullable: false,
+            },
+          ],
+        }),
+      );
 
-    await queryRunner.addColumn(
-      'ingredient',
-      new TableColumn({
-        name: 'recipeId',
-        type: 'uuid',
-      }),
-    );
+      await queryRunner.addColumn(
+        'ingredient',
+        new TableColumn({
+          name: 'recipeId',
+          type: 'uuid',
+        }),
+      );
 
-    await queryRunner.createForeignKey(
-      'ingredient',
-      new TableForeignKey({
-        columnNames: ['recipeId'],
-        referencedColumnNames: ['id'],
-        referencedTableName: 'recipe',
-        onDelete: 'CASCADE',
-      }),
-    );
+      await queryRunner.createForeignKey(
+        'ingredient',
+        new TableForeignKey({
+          columnNames: ['recipeId'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'recipe',
+          onDelete: 'CASCADE',
+        }),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
